afb186ab013cda8764cc5366aa34e05d7e6bb636
tusharr1411 <tusharr1411@gmail.com>
2024-02-20 17:36:59 +0530

    added wallet connect

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,25 @@
 import "@/styles/globals.css";
 import { MoralisProvider } from "react-moralis";
 import Header from "@/components/Header";
+import Head from "next/head";
 
 
 
 export default function App({ Component, pageProps }) {
 
   return (
+    <>
+      <Head>
+        <title> NFT Marketplace </title>
+        <meta name="description" content="NFT marketplace"/>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-    <MoralisProvider initializeOnMount={false} >
-      <Header/>
-      <Component {...pageProps} />
-    </MoralisProvider>
+      <MoralisProvider initializeOnMount={false} >
+        <Header/>
+        <Component {...pageProps} />
+      </MoralisProvider>
+    </>
 
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -5,13 +5,33 @@ import Head from "next/head";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+
+    //--------------------------------------------------------------------------------------------------------------------------
+
+    // emmited events can be accessed by offchain servieces
+    // so we will index the events off-chain and then read from our database
+    // so we need to setup a server to listen for those events to be fired, and we will add them to a database to query.
+    // woah!, but isn't it centralized ?
+    // and here comes THE Graph into play
+    // moralis is a centralized way and graph is a decenteralized way
+
+    // All our logic is still 100% on chain.
+    // Speed & Development time.
+    // Its really hard to start a prod blockchain project 100% decetralized
+    // They are working on open sourcing their code.
+    // Feature richness
+    // We can create more features with a centralized back end to start
+    // AS more decentralized tools are being created.
+    // Local development
+
+    //-----------------------------------------------------------------------------------------
+
+
+
+
     return (
         <div>
-            <Head>
-                <title> NFT Marketplace </title>
-                <meta name="description" content="NFT marketplace"/>
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
+
             Hi
         </div>
     );
