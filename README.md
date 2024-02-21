1fdd9212d6eec569c20452b8f81f574b41d34306
tusharr1411 <tusharr1411@gmail.com>
2024-02-21 21:50:20 +0530

    added moralis auth

--- README.md
+++ README.md
@@ -5,3 +5,9 @@
 2. Sell page:
     1. you can list your NFT
 
+
+
+Moralis : How do we tell it to listen to Our events ?
+1. connect it to our blockchain
+2. which contract, which events, and what to do when it hears those events
+
--- pages/_app.js
+++ pages/_app.js
@@ -2,24 +2,26 @@ import "@/styles/globals.css";
 import { MoralisProvider } from "react-moralis";
 import Header from "@/components/Header";
 import Head from "next/head";
+import { NotificationProvider } from "web3uikit";
 
-
+const APP_ID = process.env.NEXT_PUBLIC_APP_ID;
+const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL;
 
 export default function App({ Component, pageProps }) {
+    return (
+        <>
+            <Head>
+                <title> NFT Marketplace </title>
+                <meta name="description" content="NFT marketplace" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
 
-  return (
-    <>
-      <Head>
-        <title> NFT Marketplace </title>
-        <meta name="description" content="NFT marketplace"/>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <MoralisProvider initializeOnMount={false} >
-        <Header/>
-        <Component {...pageProps} />
-      </MoralisProvider>
-    </>
-
-  )
+            <MoralisProvider appId={APP_ID} serverUrl={SERVER_URL}>
+                <NotificationProvider>
+                    <Header />
+                    <Component {...pageProps} />
+                </NotificationProvider>
+            </MoralisProvider>
+        </>
+    );
 }
