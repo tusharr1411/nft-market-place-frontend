ccf3521da55434cb010442c25dc078681b3868ed
tusharr1411 <tusharr1411@gmail.com>
2024-02-24 18:28:33 +0530

    updated

--- cloudFunctions/updateActiveItems.js
+++ cloudFunctions/updateActiveItems.js
@@ -15,6 +15,25 @@ Moralis.Cloud.afterSave("ItemListed", async (request) => {
     if (confirmed) {
         logger.info("Found Item!");
         const ActiveItem = Moralis.Object.extend("ActiveItem");
+
+        // In case of listing update, search for already listed ActiveItem and delete
+        const query = new Moralis.Query(ActiveItem);
+        query.equalTo("marketplaceAddress", request.object.get("address"));
+        query.equalTo("nftAddress", request.object.get("nftAddress"));
+        query.equalTo("tokenId", request.object.get("tokenId"));
+        query.equalTo("seller", request.object.get("seller"));
+        logger.info(`Marketplace | Query: ${query}`);
+        const alreadyListedItem = await query.first();
+        if (alreadyListedItem) {
+            logger.info(`Deleting already listed ${request.object.get("objectItem")}`);
+            await alreadyListedItem.destroy();
+            logger.info(
+                `Deleting item with tokenId ${request.object.get(
+                    "tokenId"
+                )} at address ${request.object.get("address")} since it's already been listed`
+            );
+        }
+
         const activeItem = new ActiveItem();
         activeItem.set("marketplaceAddress", request.object.get("address"));
         activeItem.set("nftAddress", request.object.get("nftAddress"));
