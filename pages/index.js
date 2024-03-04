0875df79c4089e8be49de23dde2b150ce2505838
tusharr1411 <tusharr1411@gmail.com>
2024-03-04 21:09:10 +0530

    updated

--- pages/index.js
+++ pages/index.js
@@ -9,25 +9,7 @@ import GET_ACTIVE_ITEMS from "@/constants/subgraphQueries";
 import { useQuery } from "@apollo/client";
 
 export default function Home() {
-    //--------------------------------------------------------------------------------------------------------------------------
 
-    // emmited events can be accessed by offchain servieces
-    // so we will index the events off-chain and then read from our database
-    // so we need to setup a server to listen for those events to be fired, and we will add them to a database to query.
-    // woah!, but isn't it centralized ?
-    // and here comes THE Graph into play
-    // moralis is a centralized way and graph is a decenteralized way
-
-    // All our logic is still 100% on chain.
-    // Speed & Development time.
-    // Its really hard to start a prod blockchain project 100% decetralized
-    // They are working on open sourcing their code.
-    // Feature richness
-    // We can create more features with a centralized back end to start
-    // AS more decentralized tools are being created.
-    // Local development
-
-    //-----------------------------------------------------------------------------------------
     const { isWeb3Enabled, chainId } = useMoralis();
     const chainIdString = chainId ? parseInt(chainId).toString() : "31337";
     const marketplaceAddress = chainId ? networkMapping[chainIdString].NftMarketPlace[0] : null;
